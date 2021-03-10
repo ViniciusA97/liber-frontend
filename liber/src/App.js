ec3e6b08d98c6c47f8bc9d06e3b15f266f29c20e
Wagner Dantas Garcia <wagnergarcia@cc.ci.ufpb.br>
2021-03-09 21:11:42 -0300

    listagem de livros 0.0.1

--- liber/src/App.js
+++ liber/src/App.js
@@ -1,9 +1,11 @@
+import { DrawBooks } from "./components/drawBooks";
+import { ListBooks } from "./pages/listBooks";
+import './styles/geral.module.css'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          TESTE
-      </header>
+    <div className="container">
+      <ListBooks />
     </div>
   );
 }
