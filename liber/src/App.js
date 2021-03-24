6f54b868c65bd52caf633d2844ced9cd376c01f1
Wagner Dantas Garcia <wagnergarcia@cc.ci.ufpb.br>
2021-03-24 19:58:52 -0300

    "Deletar Livro"

--- liber/src/App.js
+++ liber/src/App.js
@@ -1,8 +1,11 @@
+import { DeleteBook } from "./pages/deleteBook";
+import './styles/geral.module.css'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          TESTE
+          < DeleteBook/>
       </header>
     </div>
   );
---
+++ liber/src/pages/deleteBook.js
@@ -0,0 +1,66 @@
+import { DrawBook } from "../components/drawBook";
+import axios from 'axios'
+import styles from "../styles/components/deleteBook.module.css"
+import { useEffect, useState } from "react";
+
+export function DeleteBook(){
+    const [books, setBooks] = useState(null);
+    const [carregou, setCarregou] = useState(false);
+    const [clicou, setClicou] = useState(false);
+    const [id, setID] = useState(0);
+    function searchBooks(){
+        axios.get("http://localhost:8080/api/livro/8").then(response => {
+            setBooks(response.data)
+        })
+    }
+    
+    function deleteBook(){
+        if(!clicou && carregou){
+            axios.delete(`http://localhost:8080/api/livro/delete/${books.id}`);
+            console.log(`Delete o livro do ID: ${books.id}`);
+            setClicou(true);
+        }
+    }
+
+    useEffect(() => {
+        if(!carregou){
+            searchBooks();
+            setCarregou(true);
+        }
+    })
+
+    return(
+        <div className={styles.listBooks}>
+            <div>
+                {
+                    carregou && books &&(
+                        <DrawBook 
+                            image = {books.path_foto}
+                            id = {books.id}
+                            title = {books.titulo}
+                            author = {books.autor}
+                            genre = {books.genero}
+                            avaliacao = {books.avaliacao}
+                            total_avaliacao = {books.total_avaliacao}
+                            edicao = {books.edicao}
+                            ISBN = {books.isbn}
+                            ano = {books.ano}
+                            paginas = {books.paginas}
+                            editora = {books.editora}
+                            sinopse = {books.sinopse}
+                            resenha = {books.resenha}
+                        />
+                    )
+                }
+                
+            </div>
+            <div className={styles.delete}>
+                <button onClick={deleteBook}>deletar</button>
+
+                <button>editar</button>
+                <button>voltar</button>
+            </div>
+        </div>
+    );
+}
+
