ca2ddd59caf4419e550c59405f30e87525fe8199
VanessaPessoa <vanessalima@cc.ci.ufpb.br>
2021-03-30 19:42:46 -0300

    corrigindo conflito

--- liber/src/App.js
+++ liber/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import StyledGlobal from './components/styledGlobal'
+import CadastroLivro from './pages/Livro/Cadastrar'
 function App() {
   return (
     <Fragment>
